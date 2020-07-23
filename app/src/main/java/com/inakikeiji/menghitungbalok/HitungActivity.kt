18c9642c23bed39547dfbdefb54f874f806691ad
Inaki Keiji <inakikeiji@gmail.com>
2020-07-23 14:00:53 +0700

    Hanya volume dan luas permukaan

--- app/src/main/java/com/inakikeiji/menghitungbalok/HitungActivity.kt
+++ app/src/main/java/com/inakikeiji/menghitungbalok/HitungActivity.kt
@@ -10,43 +10,17 @@ import java.lang.NumberFormatException
 
 class HitungActivity : AppCompatActivity(), View.OnClickListener {
 
-//    private lateinit var edtVolume: EditText
-//    private lateinit var edtLuasPermukaan: EditText
     private lateinit var edtPanjang: EditText
     private lateinit var edtLebar: EditText
     private lateinit var edtTinggi: EditText
     private lateinit var btnHitung: Button
     private lateinit var tvHasil: TextView
 
-//    private var hitungP_Vol:Boolean = true
-//    private var hitungL_Vol:Boolean = true
-//    private var hitungT_Vol:Boolean = true
-//
-//    private var hitungP_LuasPerm:Boolean = true
-//    private var hitungL_LuasPerm:Boolean = true
-//    private var hitungT_LuasPerm:Boolean = true
-
-//    private var panjangVL:Boolean=true
-//    private var lebarVL:Boolean=true
-//    private var tinggiVL:Boolean=true
-
     companion object{
         const val EXTRA_RUMUS="extra_rumus"
 
         const val EXTRA_HITUNG_VOL="extra_hitungVol"
         const val EXTRA_HITUNG_LUASPERM="extra_hitungLuasPerm"
-
-//        const val EXTRA_HITUNGP_VOL="extra_hitungPVol"
-//        const val EXTRA_HITUNGL_VOL="extra_hitungLVol"
-//        const val EXTRA_HITUNGT_VOL="extra_hitungTVol"
-//
-//        const val EXTRA_HITUNGP_LUASPERM="extra_hitungPLuasPerm"
-//        const val EXTRA_HITUNGL_LUASPERM="extra_hitungLLuasPerm"
-//        const val EXTRA_HITUNGT_LUASPERM="extra_hitungTLuasPerm"
-
-//        const val EXTRA_PANJANG="extra_panjang"
-//        const val EXTRA_LEBAR="extra_lebar"
-//        const val EXTRA_TINGGI="extra_tinggi"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,8 +30,6 @@ class HitungActivity : AppCompatActivity(), View.OnClickListener {
         val actionbar=supportActionBar
         actionbar!!.title="Hitung"
 
-//        edtVolume=findViewById(R.id.edt_volume)
-//        edtLuasPermukaan=findViewById(R.id.edt_luasPermukaan)
         edtPanjang=findViewById(R.id.edt_panjang)
         edtLebar=findViewById(R.id.edt_lebar)
         edtTinggi=findViewById(R.id.edt_tinggi)
@@ -69,54 +41,10 @@ class HitungActivity : AppCompatActivity(), View.OnClickListener {
         val tvRumus=findViewById<TextView>(R.id.tv_rumus)
         val rumus=intent.getStringExtra(EXTRA_RUMUS)
         tvRumus.text=rumus
-
-//        hitungP_Vol=intent.getBooleanExtra(EXTRA_HITUNGP_VOL,false)
-//        hitungL_Vol=intent.getBooleanExtra(EXTRA_HITUNGL_VOL,false)
-//        hitungT_Vol=intent.getBooleanExtra(EXTRA_HITUNGT_VOL,false)
-//
-//        hitungP_LuasPerm=intent.getBooleanExtra(EXTRA_HITUNGP_LUASPERM,false)
-//        hitungL_LuasPerm=intent.getBooleanExtra(EXTRA_HITUNGL_LUASPERM,false)
-//        hitungT_LuasPerm=intent.getBooleanExtra(EXTRA_HITUNGT_LUASPERM,false)
-
-//        panjangVL=intent.getBooleanExtra(EXTRA_PANJANG,false)
-//        lebarVL=intent.getBooleanExtra(EXTRA_LEBAR,false)
-//        tinggiVL=intent.getBooleanExtra(EXTRA_TINGGI,false)
-
-//        if(!hitungP_Vol){
-//            edtVolume.error="Field ini tidak boleh diisi"
-//        }
-//        if(!hitungL_Vol){
-//            edtVolume.error="Field ini tidak boleh diisi"
-//        }
-//        if(!hitungT_Vol){
-//            edtVolume.error="Field ini tidak boleh diisi"
-//        }
-//
-//        if(!hitungP_LuasPerm){
-//            edtLuasPermukaan.error="Field ini tidak boleh diisi"
-//        }
-//        if(!hitungL_LuasPerm){
-//            edtLuasPermukaan.error="Field ini tidak boleh diisi"
-//        }
-//        if(!hitungT_LuasPerm){
-//            edtLuasPermukaan.error="Field ini tidak boleh diisi"
-//        }
-
-//        if(panjangVL){
-//            edtPanjang.error="Field ini tidak boleh diisi"
-//        }
-//        if(lebarVL){
-//            edtLebar.error="Field ini tidak boleh diisi"
-//        }
-//        if(tinggiVL){
-//            edtTinggi.error="Field ini tidak boleh diisi"
-//        }
     }
 
     override fun onClick(v: View) {
         if(v.id==R.id.btn_hitung){
-//            val inputVolume=edtVolume.text.toString().trim()
-//            val inputLuasPermukaan=edtLuasPermukaan.text.toString().trim()
             val inputPanjang=edtPanjang.text.toString().trim()
             val inputLebar=edtLebar.text.toString().trim()
             val inputTinggi=edtTinggi.text.toString().trim()
@@ -127,20 +55,10 @@ class HitungActivity : AppCompatActivity(), View.OnClickListener {
             var fieldKosong=false
             var doubleInvalid=false
 
-//            val volume=toDouble(inputVolume)
-//            val luasPermukaan=toDouble(inputLuasPermukaan)
             val panjang=toDouble(inputPanjang)
             val lebar=toDouble(inputLebar)
             val tinggi=toDouble(inputTinggi)
 
-//            if(inputVolume.isEmpty()&&(hitungP_Vol||hitungL_Vol||hitungT_Vol)){
-//                fieldKosong=true
-//                edtVolume.error="Field ini tidak boleh kosong"
-//            }
-//            if(inputLuasPermukaan.isEmpty()&&(hitungP_LuasPerm||hitungL_LuasPerm||hitungT_LuasPerm)){
-//                fieldKosong=true
-//                edtLuasPermukaan.error="Field ini tidak boleh kosong"
-//            }
             if(inputPanjang.isEmpty()){
                 fieldKosong=true
                 edtPanjang.error="Field ini tidak boleh kosong"
@@ -155,56 +73,19 @@ class HitungActivity : AppCompatActivity(), View.OnClickListener {
             }
 
 
-
-//            if(volume==null&&inputVolume.isNotEmpty()&&(hitungP_Vol||hitungL_Vol||hitungT_Vol)){
-//                doubleInvalid=true
-//                edtVolume.error="Field ini harus berupa nomer yang valid"
-//            }
-//            if(luasPermukaan==null&&inputLuasPermukaan.isNotEmpty()&&(hitungP_LuasPerm||hitungL_LuasPerm||hitungT_LuasPerm)){
-//                doubleInvalid=true
-//                edtLuasPermukaan.error="Field ini harus berupa nomer yang valid"
-//            }
-            if(panjang==null&&inputPanjang.isNotEmpty()
-//                &&!panjangVL
-            ){
+            if(panjang==null&&inputPanjang.isNotEmpty()){
                 doubleInvalid=true
                 edtPanjang.error="Field ini harus berupa nomer yang valid"
             }
-            if(lebar==null&&inputLebar.isNotEmpty()
-//                &&!lebarVL
-            ){
+            if(lebar==null&&inputLebar.isNotEmpty()){
                 doubleInvalid=true
                 edtLebar.error="Field ini harus berupa nomer yang valid"
             }
-            if(tinggi==null&&inputTinggi.isNotEmpty()
-//                &&!tinggiVL
-            ){
+            if(tinggi==null&&inputTinggi.isNotEmpty()){
                 doubleInvalid=true
                 edtTinggi.error="Field ini harus berupa nomer yang valid"
             }
 
-
-//            if(inputVolume.isNotEmpty()&&!hitungP_Vol&&!hitungL_Vol&&!hitungT_Vol){
-//                edtVolume.error="Field ini seharusnya kosong (tidak dihitung)"
-//            }
-//            if(inputLuasPermukaan.isNotEmpty()&&!hitungP_LuasPerm&&!hitungL_LuasPerm&&!hitungT_LuasPerm){
-//                edtLuasPermukaan.error="Field ini seharusnya kosong (tidak dihitung)"
-//            }
-//            if(inputPanjang.isNotEmpty()
-////                &&panjangVL
-//            ){
-//                edtPanjang.error="Field ini seharusnya kosong (tidak dihitung)"
-//            }
-//            if(inputLebar.isNotEmpty()
-////                &&lebarVL
-//            ){
-//                edtLebar.error="Field ini seharusnya kosong (tidak dihitung)"
-//            }
-//            if(inputTinggi.isNotEmpty()
-////                &&tinggiVL
-//            ){
-//                edtTinggi.error="Field ini seharusnya kosong (tidak dihitung)"
-//            }
 
             if(!fieldKosong&&!doubleInvalid){
                 if(hitungVol){
@@ -216,32 +97,6 @@ class HitungActivity : AppCompatActivity(), View.OnClickListener {
                         2 * (panjang as Double * lebar as Double + panjang * tinggi as Double + lebar * tinggi)
                     tvHasil.text = luasPermukaan.toString()
                 }
-
-//                if(hitungP_Vol){
-//                    val panjang=volume as Double/(lebar as Double*tinggi as Double)
-//                    tvHasil.text=panjang.toString()
-//                }
-//                if(hitungL_Vol){
-//                    val lebar=volume as Double/(panjang as Double*tinggi as Double)
-//                    tvHasil.text=lebar.toString()
-//                }
-//                if(hitungT_Vol){
-//                    val tinggi=volume as Double/(panjang as Double*lebar as Double)
-//                    tvHasil.text=tinggi.toString()
-//                }
-//
-//                if(hitungP_LuasPerm){
-//                    val panjang=(luasPermukaan as Double/2-lebar as Double*tinggi as Double)/(lebar+tinggi)
-//                    tvHasil.text=panjang.toString()
-//                }
-//                if(hitungL_LuasPerm){
-//                    val lebar=(luasPermukaan as Double/2-panjang as Double*tinggi as Double)/(panjang+tinggi)
-//                    tvHasil.text=lebar.toString()
-//                }
-//                if(hitungT_LuasPerm){
-//                    val tinggi=(luasPermukaan as Double/2-panjang as Double*lebar as Double)/(panjang+lebar)
-//                    tvHasil.text=tinggi.toString()
-//                }
             }
         }
     }
--- app/src/main/java/com/inakikeiji/menghitungbalok/MainActivity.kt
+++ app/src/main/java/com/inakikeiji/menghitungbalok/MainActivity.kt
@@ -15,22 +15,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val btnSifat:Button=findViewById(R.id.btn_sifat)
         val btnVolume:Button=findViewById(R.id.btn_volume)
         val btnLuasPermukaan:Button=findViewById(R.id.btn_luasPermukaan)
-//        val btnPanjang:Button=findViewById(R.id.btn_panjang)
-//        val btnLebar:Button=findViewById(R.id.btn_lebar)
-//        val btnTinggi:Button=findViewById(R.id.btn_tinggi)
-//        val btnDiagonalBidangSisi:Button=findViewById(R.id.btn_diagonalBidangSisi)
-//        val btnDiagonalRuang:Button=findViewById(R.id.btn_diagonalRuang)
-//        val btnLuasBidangDiagonal:Button=findViewById(R.id.btn_luasBidangDiagonal)
 
         btnSifat.setOnClickListener(this)
         btnVolume.setOnClickListener(this)
         btnLuasPermukaan.setOnClickListener(this)
-//        btnPanjang.setOnClickListener(this)
-//        btnLebar.setOnClickListener(this)
-//        btnTinggi.setOnClickListener(this)
-//        btnDiagonalBidangSisi.setOnClickListener(this)
-//        btnDiagonalRuang.setOnClickListener(this)
-//        btnLuasBidangDiagonal.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -53,45 +41,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 pindahHitungLuasPermAct.putExtra(HitungActivity.EXTRA_HITUNG_LUASPERM,true)
                 startActivity(pindahHitungLuasPermAct)
             }
-
-//            R.id.btn_panjang->{
-//                val pindahPanjangAct=Intent(this,PilPLTActivity::class.java)
-//                pindahPanjangAct.putExtra(PilPLTActivity.EXTRA_PILP_VOL,true)
-//                pindahPanjangAct.putExtra(PilPLTActivity.EXTRA_PILP_LUASPERM,true)
-//                pindahPanjangAct.putExtra(PilPLTActivity.EXTRA_PILPANJANG,true)
-//                startActivity(pindahPanjangAct)
-//            }
-//
-//            R.id.btn_lebar->{
-//                val pindahLebarAct=Intent(this,PilPLTActivity::class.java)
-//                pindahLebarAct.putExtra(PilPLTActivity.EXTRA_PILL_VOL,true)
-//                pindahLebarAct.putExtra(PilPLTActivity.EXTRA_PILL_LUASPERM,true)
-//                pindahLebarAct.putExtra(PilPLTActivity.EXTRA_PILLEBAR,true)
-//                startActivity(pindahLebarAct)
-//            }
-//
-//            R.id.btn_tinggi->{
-//                val pindahTinggiAct=Intent(this,PilPLTActivity::class.java)
-//                pindahTinggiAct.putExtra(PilPLTActivity.EXTRA_PILT_VOL,true)
-//                pindahTinggiAct.putExtra(PilPLTActivity.EXTRA_PILT_LUASPERM,true)
-//                pindahTinggiAct.putExtra(PilPLTActivity.EXTRA_PILTINGGI,true)
-//                startActivity(pindahTinggiAct)
-//            }
-//
-//            R.id.btn_diagonalBidangSisi->{
-//                val pindahDiagonalBidangSisiAct=Intent(this,DiagonalBidangSisiActivity::class.java)
-//                startActivity(pindahDiagonalBidangSisiAct)
-//            }
-//
-//            R.id.btn_diagonalRuang->{
-//                val pindahDiagonalRuangAct=Intent(this,DiagonalRuangActivity::class.java)
-//                startActivity(pindahDiagonalRuangAct)
-//            }
-//
-//            R.id.btn_luasBidangDiagonal->{
-//                val pindahLuasBidangDiagonalAct=Intent(this,LuasBidangDiagonalActivity::class.java)
-//                startActivity(pindahLuasBidangDiagonalAct)
-//            }
         }
     }
 }
